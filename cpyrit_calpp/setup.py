70b310273dcc31bedb9621c3abfd1784a875de88
MP <mainuser@DEKA>
2019-08-27 06:02:57 -0400

    Fix environment variable

--- cpyrit_calpp/setup.py
+++ cpyrit_calpp/setup.py
@@ -32,14 +32,16 @@ import sys
 CALPP_INC_DIRS = []
 
 try:
-    CALPP_INC_DIR = os.environ['ATISTREAMSDKROOT']
-    # CALPP_INC_DIR = "/opt/AMDAPPSDK-3.0/include/CAL/"
+    if "AMDAPPSDKROOT" in os.environ:
+        CALPP_INC_DIR = os.environ['AMDAPPSDKROOT']
+    else:
+        CALPP_INC_DIR = "/opt/AMDAPPSDK-3.0"
 except KeyError:
-    print >>sys.stderr, "unavailable enviroment variable ATISTREAMSDKROOT"
+    print >>sys.stderr, "unavailable enviroment variable AMDAPPSDKROOT"
     raise
 else:
     if os.path.exists(CALPP_INC_DIR):
-        CALPP_INC_DIRS.append(os.path.join(CALPP_INC_DIR, 'include/CAL'))
+        CALPP_INC_DIRS.append(os.path.join(CALPP_INC_DIR, '/include/CAL'))
     else:
         print >>sys.stderr, "The headers required to build CAL++ kernel" \
                             "were not found. Trying to continue anyway..."
